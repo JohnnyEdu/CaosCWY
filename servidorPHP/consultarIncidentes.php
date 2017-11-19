35004c0b6ad6fd964bb5903a4d9325ae6301c7b6
Jonathan Baez <johnny032295@gmail.com>
2017-11-19 12:56:03 -0300

    subo server

--- servidorPHP/consultarIncidentes.php
+++ servidorPHP/consultarIncidentes.php
@@ -9,20 +9,21 @@
  	
  	//para que traiga la fecha en español
  	mysqli_query($conn, "SET lc_time_names = 'es_AR'");
- 	
-	$sql = "SELECT COUNT(*) as cantidad,tipo,zona,latitud,longitud FROM incidentes GROUP BY tipo,latitud,longitud HAVING latitud = ".$_POST["latitud"]." AND longitud = ".$_POST["longitud"];
-	$result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
+ 	/***************Se comenta esta consulta, ya que el agrupamiento lo hacemos en android ***********/
+	/*$sql = "SELECT COUNT(*) as cantidad,tipo,zona,latitud,longitud FROM incidentes GROUP BY tipo,latitud,longitud HAVING latitud = ".$_POST["latitud"]." AND longitud = ".$_POST["longitud"];
+	$result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));*/
 
 	 //create an array
-    $emparray = array();
-    while($row =mysqli_fetch_assoc($result))
+    //$emparray = array();
+    /*while($row =mysqli_fetch_assoc($result))
     {
         array_push($emparray,$row);
-    }
+    }*/
 
     mysqli_query($conn, "SET lc_time_names = 'es_AR'");
     
-    $sql = "SELECT id,usuario,DATE_FORMAT(fechayhora,'%d %M %Y  %h:%m:%s') fechaYhora,tipo,zona,comentarios,latitud,longitud FROM incidentes WHERE latitud = ".$_POST["latitud"]." AND longitud = ".$_POST["longitud"];
+    
+    $sql = "SELECT incs.id,incs.usuario,DATE_FORMAT(fechayhora,'%d %M %Y  %h:%m:%s') fechaYhora,tipo,zona,comentarios,latitud,longitud,imgs.imagen FROM incidentes incs JOIN imagenes imgs ON imgs.id_incidente = incs.id WHERE latitud = ".$_POST["latitud"]." AND longitud = ".$_POST["longitud"];
 	$result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
 
     $sinagrupararray = array();
@@ -31,7 +32,7 @@
         array_push($sinagrupararray,$row);
     }
     $respuesta = new stdClass();
-    $respuesta->agrupada = $emparray;
+    //$respuesta->agrupada = $emparray;
     $respuesta->sinagrupar = $sinagrupararray;
      echo json_encode($respuesta);
 ?>
