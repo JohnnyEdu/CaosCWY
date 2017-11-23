a459bac0377bb3be4ee09c9d17b1541d362f8cbe
JohnnyEdu <johnny032295@gmail.com>
2017-11-23 13:43:33 -0300

    Update consultarIncidentes.php

--- servidorPHP/consultarIncidentes.php
+++ servidorPHP/consultarIncidentes.php
@@ -29,8 +29,8 @@
 	    //-35, -58
 	$latitudArea = floatval($_POST["latitud"]);
 	$longitudArea = floatval($_POST["longitud"]);
-    	$where = " WHERE i.latitud  BETWEEN  (".$_POST["latitud"]." -200) AND (".$_POST["longitud"]."+200) AND i.longitud BETWEEN
-	(".$_POST["longitud"]."-200) AND (".$_POST["latitud"]." +200");
+    	$where = " WHERE i.latitud  BETWEEN  (".$_POST["latitud"]." -200) AND (".$_POST["longitud"]."+200) AND i.longitud BETWEEN 
+	(".$_POST["longitud"]."-200) AND (".$_POST["latitud"]." +200")";
     }
     //$sql = "SELECT incs.id,incs.usuario,DATE_FORMAT(fechayhora,'%d %M %Y  %h:%m:%s') fechaYhora,tipo,zona,comentarios,latitud,longitud,imgs.imagen FROM incidentes incs JOIN imagenes imgs ON imgs.id_incidente = incs.id".$where; 
 
